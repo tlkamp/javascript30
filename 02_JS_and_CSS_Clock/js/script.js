1c668edbc9792c8cf0221285f9250691f7e70997
Traci <traci.kamp@blackburn.edu>
2018-04-15 18:30:48 -0500

    Make the clock hands not freak out when they reach 360 degrees

--- 02_JS_and_CSS_Clock/js/script.js
+++ 02_JS_and_CSS_Clock/js/script.js
@@ -7,16 +7,27 @@ const setDate = () => {
 
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
+  handleFlicker(secondHand, secondsDegrees);
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
   const minsDegrees = (mins / 60) * 360 + 90;
+  handleFlicker(minuteHand, minsDegrees);
   minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hours = now.getHours();
   const hoursDegrees = (hours / 12) * 360 + 90;
+  handleFlicker(hourHand, hoursDegrees);
   hourHand.style.transform =`rotate(${hoursDegrees}deg)`;
 };
+
+const handleFlicker = (hand, degrees) => {
+  if(degrees === 90) {
+    hand.style.transition = 'all 0.0s';
+  } else {
+    hand.style.transition = 'all 0.2s';
+  }
+}
 
 setInterval(setDate, 1000);
 setDate();
